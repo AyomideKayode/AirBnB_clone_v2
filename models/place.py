b508809168c17322ea780e591163bddb52b0d3c6
Ayomide Kayode <ayomidekay7@gmail.com>
2023-12-19 16:24:13 +0100

    DBStorage - Amenity... and BOOM🚧

--- models/place.py
+++ models/place.py
@@ -4,13 +4,24 @@
 """
 
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from os import getenv
 from sqlalchemy.orm import relationship
 import models
 from models.review import Review
+from models.amenity import Amenity
 
 storage_type = getenv("HBNB_TYPE_STORAGE")
+
+# association table for Many-to-Many relationship
+# between places and amenities
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True, nullable=False))
 
 
 class Place(BaseModel):
@@ -32,6 +43,9 @@ class Place(BaseModel):
         longitude = Column(Float, nullable=True)
         reviews = relationship("Review", backref="place",
                                cascade="all, delete, delete-orphan")
+        amenities = relationship("Amenity", secondary="place_amenity",
+                                 viewonly=False,
+                                 back_populates="place_amenities")
     else:
         city_id = ""
         user_id = ""
@@ -47,7 +61,23 @@ class Place(BaseModel):
 
         @property
         def reviews(self):
-            """Getter attribute that returns the list of Review instances with place_id
-                equals to the current Place.id"""
+            """Getter attribute that returns the list of Review instances
+            with place_id equals to the current Place.id
+            """
             return [review for review in models.storage.all(Review)
-                if review.place_id == self.id]
+                    if review.place_id == self.id]
+
+        @property
+        def amenities(self):
+            """Getter attribute amenities for FileStorage"""
+            list_amenities = []
+            for amenity in models.storage.all(Amenity).values():
+                if amenity.place_id == self.amenity_ids:
+                    list_amenities.append(amenity)
+            return list_amenities
+
+        @amenities.setter
+        def amenities(self, amenity):
+            """Setter attribute amenities for FileStorage"""
+            if isinstance(amenity, Amenity):
+                self.amenity_ids.append(amenity.id)
