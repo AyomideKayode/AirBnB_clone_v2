eecec01950c3c3e77306124fb6e3bbdaff9b2ab5
Ayomide Kayode <ayomidekay7@gmail.com>
2023-12-19 11:24:58 +0100

    DBStorage User - Hot fix.

--- models/user.py
+++ models/user.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
 """Defines the User class."""
-from models.base_model import Basei, BaseModel
+
+from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
